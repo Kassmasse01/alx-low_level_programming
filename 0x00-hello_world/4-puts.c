eb9f95db4b7ec3d324e2d143d5096669ed152a15
Kassmasse01 <99286610+Kassmasse01@users.noreply.github.com>
2022-03-13 12:55:27 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ *main - main
+ *Return: 0
+ *Description - outputs string
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+        puts("Programming is like building a multilingual puzzle\n");
+        return (0);
 }
+
