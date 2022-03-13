db8383cafd46560ad712f174db1c0490d6503cef
Kassahun Getachew Gelan <kai@shadow>
2022-03-14 01:16:12 +0300

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *Return: 0
+ *Description - prints lower case alphabet without "e & q"
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+		continue;
+		}
+	putchar(n);
+	n++;
+	}
+	putchar(10);
+	return (0);
+}
