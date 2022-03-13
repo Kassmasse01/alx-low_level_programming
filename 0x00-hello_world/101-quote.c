4fea3ca438d1ecd865796bac2af60a2bdf3c5046
Kassahun Getachew Gelan <kai@shadow>
2022-03-13 17:21:18 +0300

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ *main - main
+ *Return: 0
+ *Description - outputs string
+ */
+int main()
+{
+        putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+        return (1);
+}
