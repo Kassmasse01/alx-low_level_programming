36c032436e443f56a74ad45f8cc519617eda1627
Kassahun Getachew Gelan <kai@shadow>
2022-03-13 17:51:14 +0300

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<string.h>
+#include<unistd.h>
 /**
  *main - main
- *Return: 0
+ *Return: 1
  *Description - outputs string
  */
 int main()
 {
-        putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
