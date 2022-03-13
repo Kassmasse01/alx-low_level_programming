87a72e1a524881a26338043f8f4caffefb3a17ec
Kassahun Getachew Gelan <kai@shadow>
2022-03-13 14:20:46 +0300

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
-  int main( void )
-   {
-      printf("with proper grammar, but the outcome is a piece of art," );
-   } 
+#include<stdio.h>
+/**
+ *main - outputs string
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
