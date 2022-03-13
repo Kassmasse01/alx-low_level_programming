011052dd41bd62bf57314b8895e27c48a65b56f6
Kassahun Getachew Gelan <kai@shadow>
2022-03-13 13:47:49 +0300

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,12 +7,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
-=======
-                puts("\"Programming is like building a multilingual puzzle");
-                return (0);
->>>>>>> 0d24d7fc2f837061601e0391c7d49b207a5c1b1d
 }
-
