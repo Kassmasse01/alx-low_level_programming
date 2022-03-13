9ab1f88c489336fa89544d64a31ff1976208ca2e
Kassahun Getachew Gelan <kai@shadow>
2022-03-14 00:42:51 +0300

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *Return: 0
+ *Description - prints lower case alphabet
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+	putchar(n);
+	n++;
+	}
+	return (0);
+}
