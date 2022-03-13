6025b5f77bd503285600ffde8fe0c1d72e00143c
Kassahun Getachew Gelan <kai@shadow>
2022-03-14 00:59:16 +0300

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *Return: 0
+ *Description - prints lower case and upper case alphabet
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+	putchar(n)
+	n++;
+	}
+	while (m <= 90)
+	{
+	putchar(m);
+	m++;
+	}
+	putchar(10);
+	return (0);
+}
