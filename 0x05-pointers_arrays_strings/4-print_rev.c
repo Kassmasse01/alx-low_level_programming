f751572561e6b42c894f3f0358a5c30989d1cb7a
Kassahun Getachew Gelan <kai@shadow>
2022-03-22 02:19:20 +0300

    task 6

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string, followed by a new line.
- * @s: input string to print.
+ * print_rev - check the code for Holberton School students.
+ * @s: String to be reverse printed
+ * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	int i;
 
+	i = 0;
+
 	while (s[i] != '\0')
-	{
 		i++;
-	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
