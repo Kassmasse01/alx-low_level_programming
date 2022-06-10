6f973910ed3050cc55c46d14d40f414b37d6ec9d
Freedom <freedom@localhost.localdomain>
2022-06-10 03:28:11 +0300

    new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,57 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts node at index
- * @h: head of node
- * @idx: index to insert node
- * @n: data for new node
- * Return: list with inserted node
+ * insert_node - insert node at given index
+ * @tmp: ptr to nth position node in doubly linked list
+ * @n: node data
+ * Return: address of inserted node
+ */
+dlistint_t *insert_node(dlistint_t *tmp, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	tmp->prev = new;
+
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - create and insert node at nth index
+ * @h: pointer to head of list
+ * @idx: index
+ * @n: node data
+ * Return: address of inserted node, or NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 1;
-	dlistint_t *temp = NULL, *new = NULL;
+	dlistint_t *tmp;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || h == NULL)
-		return (NULL);
-	new->n = n;
-	temp = *h;
+	/* insert at beginning if empty or existing linked list */
 	if (idx == 0)
-	{
-		*h = new;
-		new->next = temp;
-		new->prev = NULL;
-		temp->prev = new;
-		return (new);
-	}
-	while (temp->next != NULL)
-	{
-		if (count == idx) /* found back */
-		{
-			new->prev = temp; /* current prev to back link */
-			new->next = temp->next; /* current next to front link*/
-			temp->next = new; /* back next link */
-			new->next->prev = new; /* from prev link */
-		}
-		temp = temp->next;
-		count++;
-	}
-	if (count == idx) /* end of DLL */
-	{
-		new->prev = temp; /* current prev to back link */
-		new->next = NULL; /* current next to NULL*/
-		temp->next = new; /* back next link */
-	}
-	if (count < idx)
-	{
-		free(new);
+		return (add_dnodeint(h, n));
+	if (!h)
 		return (NULL);
+
+	/* insert in the middle of list */
+	tmp = *h;
+	while ((idx != 0) && (tmp->next))
+	{
+		idx -= 1;
+		tmp = tmp->next;
+		if (idx == 0)
+			return (insert_node(tmp, n));
 	}
-	return (new);
+
+	/* insert at the end of list if idx is one after last node */
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
+	return (NULL);
 }
